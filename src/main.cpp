21e39a5c445a1f0019a92d95224f19dc3ba13e90
EmanuelAlbornoz <ealbornoz@itba.edu.ar>
2025-05-08 20:04:06 -0300

    Lectura Serial y GPS

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,42 @@
 #include <Arduino.h>
+//Funciones:
+void serialEvent();
 
-// SPI, I2C, que otras librerias default?
+// UART por software para GPS en pines 2 (RX) y 3 (TX)
+UART GPS(2, 3);
 
-void setup()
-{
-	Serial.begin(115200);
+unsigned long previousMillis = 0;
+const unsigned long interval = 1000; // 1 segundo
+
+void setup() {
+  Serial.begin(115200);  // Consola Serial
+  while (!Serial);
+
+  Serial1.begin(115200); // UART física integrada
+  GPS.begin(115200);     // UART por software (GPS)
+
+  Serial.println("Iniciando prueba de UART a 115200 baudios...");
 }
 
-void loop()
-{
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    Serial1.print("HOLA_UART1\n");
+    GPS.print("HOLA_GPS\n");
+  }
+  serialEvent();
 
+}
+
+void serialEvent() {
+  
+ // Lectura desde UART física integrada
+  if (Serial1.available()) {
+    Serial.println(Serial1.read());
+  }
+  // Lectura desde UART GPS
+  if (GPS.available()) {
+    Serial.println(GPS.read());
+  }
 }
