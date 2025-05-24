52cd1449fef9da959f169a90a60d374e7d39af53
Rafael Dalzotto <rafaeldalzotto2016@gmail.com>
2025-05-24 17:48:40 -0300

    add test data

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@ mbed::DigitalOut *gpio_pin = nullptr;   // gpio pin for time measurement purpose
 // UART gps(D3_RX, D2_TX); // GPS Serial
 // UART gps(D2_TX, D3_RX); // GPS Serial
 
+#include "gps_test_data.h"
+uint8_t index_connected = 0;
+uint8_t index_disconnected = 0;
+unsigned long last_millis = 0;
+
 void setup()
 {
 	Serial.begin(115200); // Consola Serial
@@ -29,5 +34,28 @@ void setup()
 
 void loop()
 {
+	if( millis() - last_millis > 1000 )
+	{
+		gpio_pin->write(1);
+		if (index_connected < GPS_CONNECTED_TEST_DATA_LEN)
+		{
+			Serial.print(gps_test_data_connected[index_connected]);
+			index_connected++;
+		}
+		else if (index_disconnected < GPS_DISCONNECTED_TEST_DATA_LEN)
+		{
+			Serial.print(gps_test_data_disconnected[index_disconnected]);
+			index_disconnected++;
+		}
+		else
+		{
+			Serial.println("\nFIN DEL TEST\n");
+			delay(2000);
+			index_connected = 0;
+			index_disconnected = 0;
+		}
+		gpio_pin->write(0);
+		last_millis = millis();
+	}
 	// Leer GPS ?
 }
