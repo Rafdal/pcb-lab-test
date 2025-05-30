a42ec0b26ada47ee6951a411bf9cd5fca43cbb26
Santino Agosti <santinoaggosti@gmail.com>
2025-05-30 11:27:56 -0300

    Archivos iniciales, Funciones de computo de orientacion en archivos Orientation.cpp y orientation.h

---
+++ src/Orientation.cpp
@@ -0,0 +1,15 @@
+#include "Orientation.h"
+static float xMag, yMag;
+
+// Testing Offset
+//float offsetX = -38.500000;
+//float offsetY = 23.500000;
+
+// Devuelve en formato float la orientacion del CANSAT en grados [0, 360]°
+float computeHeading(float &x, float &y, float &offsetX, float offsetY) {
+    float heading = 0;
+  float heading = atan2(y-offsetY, x-offsetX)*180.0 / M_PI + 90; // En grados
+        if (heading < 0) heading += 360.0;
+  return heading;
+}
+
---
+++ src/Orientation.h
@@ -0,0 +1,9 @@
+#ifndef ORIENTATION_H
+#define ORIENTATION_H
+
+#include <Arduino.h>
+#include <math.h>
+
+float computeHeading(float &x, float &y, float &offsetX, float &offsetY);
+
+#endif // ORIENTATION_H
